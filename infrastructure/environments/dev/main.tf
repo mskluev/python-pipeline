27d153e98bc480ebb6e592e1ca98fb9f8141c69b
Mike Kluever <mskluev@skogul.com>
2026-04-25 13:04:17 -0400

    WIP

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -23,3 +23,44 @@ variable "commit_sha" {
 }
 
 # Add module calls here
+
+variable "permissions_boundary" {
+  description = "aws permissions boundary"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "aws subnet ids"
+  type        = list(string)
+}
+
+variable "security_group_ids" {
+  description = "aws security group ids"
+  type        = list(string)
+}
+
+variable "model_s3_path" {
+  description = "S3 path to the model.tar.gz file"
+  type        = string
+}
+
+variable "sagemaker_iam_role" {
+  description = "ARN of the sagemaker iam role to use"
+  type        = string
+}
+
+variable "triton_image_uri" {
+  description = "Triton docker image"
+  type        = string
+}
+
+module "sagemaker_skill" {
+  source = "../../modules/sagemaker_skill"
+
+  permissions_boundary = var.permissions_boundary
+  subnet_ids           = var.subnet_ids
+  security_group_ids   = var.security_group_ids
+  model_s3_path        = var.model_s3_path
+  sagemaker_iam_role   = var.sagemaker_iam_role
+  triton_image_uri     = var.triton_image_uri
+}
